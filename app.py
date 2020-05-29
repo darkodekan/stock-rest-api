ebe798c8a684dd9b099e309776853bd0540ea5aa
darko <darko.dekan@protonmail.com>
2020-05-29 20:01:57 +0200

    modifying spaces and tabs

--- app.py
+++ app.py
@@ -33,44 +33,41 @@ UPDATE_SERVER_ID[1]=0
 UPDATE_SERVER_ID[2]=0
 UPDATE_SERVER_ID[3]=0
 
-for server in servers_id:
-	UPDATE_SERVER_ID[
-
 class Product():
-	def __init__(self, url, server_id):
-		self.url = url
+	def __init__(self, product_id, server_id):
+		self.product_id = product_id
 		self.server_id = server_id
 
 #find location of link
 def get_url_server_id(product_url):
 	try:
-    	conn = mysql.connect
-    	cursor = conn.cursor()
-    	servers_url = {}
-    	sql = "SELECT * FROM products WHERE url=%s"
-    	cursor.execute(sql, (product_url,))
-    	product_row = cursor.fetchone()
+		conn = mysql.connect
+		cursor = conn.cursor()
+		servers_url = {}
+		sql = "SELECT * FROM product WHERE url=%s"
+		cursor.execute(sql, (product_url,))
+		product_row = cursor.fetchone()
 		cursor.close()
 		conn.close()
-    	if product_row:
-       		return product_row["server_id"]
-    	else:
-       		return None
+		if product_row:
+			return product_row["server_id"]
+		else:
+			return None
 	except Exception as e:
 		return None
 #display contents of database
 def get_all_products():
 	try:
 		products = list()
-    	conn = mysql.connect
-    	cursor = conn.cursor()
-    	cursor.execute("SELECT * FROM products");
-    	product_rows = cursor.fetchall()
-    	cursor.close()
-    	conn.close()
+		conn = mysql.connect
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM product");
+		product_rows = cursor.fetchall()
+		cursor.close()
+		conn.close()
 		for product in product_rows:
-			products.append(Product(row["url"], row["server_id"])
-    	return products
+			products.append(Product(row["url"], row["server_id"]))
+		return products
 	except Exception as e:
 		return None
 
@@ -79,10 +76,10 @@ def get_server_products(server_id):
 		products = list()
 		conn = mysql.connect
 		cursor = conn.cursor()
-		cursor.execute("SELECT FROM products WHERE server_id=%s", (server_id,))
+		cursor.execute("SELECT FROM product WHERE server_id=%s", (server_id,))
 		products_rows = cursor.fetchall()
 		for row in products_row:
-			products.append(Product(row["url"], row["server_id"])	
+			products.append(Product(row["url"], row["server_id"]))	
 		cursor.close()
 		conn.close()
 		return products
@@ -94,43 +91,43 @@ def is_product_exists(url):
 		products = list()
 		conn = mysql.connect
 		cursor = conn.cursor()
-		cursor.execute("SELECT FROM products WHERE url=%s", (url,))
+		cursor.execute("SELECT FROM product WHERE url=%s", (url,))
 
 	except Exception as e:
 		return None
 #add try and catch
 def add_to_database(product):
 	try:
-        conn = mysql.connect
-        cursor = conn.cursor()
-        sql = "INSERT INTO products(url, server_id) VALUES(%s,%s)"
-        cursor.execute(sql, (product.url, product.server_id))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return True
+		conn = mysql.connect
+		cursor = conn.cursor()
+		sql = "INSERT INTO product(url, server_id) VALUES(%s,%s)"
+		cursor.execute(sql, (product.url, product.server_id))
+		conn.commit()
+		cursor.close()
+		conn.close()
+		return True
 	except Exception as e:
 		print(e)
 		return False
 
 def delete_from_database(product):  
-    try:
-        conn = mysql.connect
-        cursor = conn.cursor()
-        sql = "DELETE FROM products WHERE url=%s"
-        cursor.execute(sql, product.url)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return True
+	try:
+		conn = mysql.connect
+		cursor = conn.cursor()
+		sql = "DELETE FROM product WHERE url=%s"
+		cursor.execute(sql, product.url)
+		conn.commit()
+		cursor.close()
+		conn.close()
+		return True
 	except:
-    	return False
+		return False
 
 def update_database(product1, product2):
 	try:
 		conn = mysql.connect
 		cursor = conn.cursor()
-		sql = "UPDATE products SET url=%s WHERE url=%s"
+		sql = "UPDATE product SET url=%s WHERE url=%s"
 		cursor.execute(sql, product.url, product.url)
 		conn.commit()
 		cursor.close()
@@ -142,7 +139,7 @@ def update_database(product1, product2):
 def login(login_info):
     conn = mysql.connect
     cursor = conn.cursor()
-    sql = "SELECT * FROM admins WHERE username=%s AND password=%s"
+    sql = "SELECT * FROM admin WHERE username=%s AND password=%s"
     username = admin_config["username"]
     password = admin_config["password"]
     cursor.execute(sql, (username, password))
@@ -158,25 +155,25 @@ def add(command):
 	exists = is_product_exists(command["url"])
 	if exists==True:
 		success = None
-        if command["server"]=="random":
-             success = add_to_database(Product(command["url"], random.randint(1,3))
-        else:
-             success = add_to_database(Product(command["url"], num_server=command["server"]))
-		if success:
-			 return {"success":"true"}
+		if command["server"]=="random":
+			 success = add_to_database(Product(command["url"], random.randint(1,3)))
 		else:
-			return {"success":"false"}
-	else if exists == False:
-		return {"success":"exists"}
-	return {"success":"false"}
-def delete(command):
-	if exists == False:
-    	success = delete_from_database(command["url"])
+			 success = add_to_database(Product(command["url"], num_server=command["server"]))
 		if success:
 			return {"success":"true"}
 		else:
 			return {"success":"false"}
-	else if exists == True:
+	elif exists == False:
+		return {"success":"exists"}
+	return {"success":"false"}
+def delete(command):
+	if exists == False:
+		success = delete_from_database(command["url"])
+		if success:
+			return {"success":"true"}
+		else:
+			return {"success":"false"}
+	elif exists == True:
 		return {"success":"exists"}
 	return {"success":"false"}
 
@@ -190,32 +187,33 @@ def get_url_list():
 	else:
 		{"product_url_list":"fail"}
 def get_server_update_id(id):
-	
+	pass	
 	
 class Introduction(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template("index.html"),200,headers)
 
-class Database(Resource):
-    def get(self):
-        return {"message":get_all_links()}
+class Products(Resource):
+	def get(self):
+		return {"message":get_all_links()}
 
-    def post(self):
-        command = request.get_json()
+	def post(self):
+		command = request.get_json()
 		exists = is_product_exists(command["url"])
-        if command["action"] == "add":
+		if command["action"] == "add":
 			add(command,exists)
-        elif command["action"] == "update":
+		elif command["action"] == "update":
 			update(command,exists)
-        elif command["action"] == "delete":
+		elif command["action"] == "delete":
 			delete(command,exists)
 		elif command["action"] == "get_url_list":
 			get_url_list(command, exists)
 		elif command["action"] == "check_update":
 			return update_id[command["server_id"]]
 			
-
+class Product(Resource):
+	pass
 class Login(Resource):
 	def post(self):
 		login_json = request.get_json()
@@ -225,8 +223,9 @@ class Login(Resource):
 			return {"success":False}
 
 api.add_resource(Introduction, "/")
-api.add_resource(Database, "/database")
-api.add_resource(ServerRetrieval, "/database/<int:num>")
+api.add_resource(Products, "/products")
+api.add_resource(Product, "/products/<string:product_id")
+api.add_resource(ServerRetrieval, "/products/server/<int:num>")
 api.add_resource(Login, "/login")
 
 if __name__ == "__main__":
